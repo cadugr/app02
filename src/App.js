6c22611cfac0d97094c25a1dc8a54be2708c2f84
cadugr <cadu.guerra@gmail.com>
2019-06-03 07:47:34 -0300

    Provendo diversas novas implementações na aplicação

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Fragment } from 'react';
-import styled from 'styled-components';
 import GlobalStyle from './styles/global';
-
-const Title = styled.h1`
-  color: #f00;
-  font-size: 32px;
-`;
+import Main from './pages/Main/index';
 
 const App = () => (
   <Fragment>
     <GlobalStyle />
-    <div className="App" />
+    <Main />
   </Fragment>
 );
 
